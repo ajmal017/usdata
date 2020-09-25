ef6eeebaa143c7dfab5ee9d214a3388745b29936
peepee <ppark9553@gmail.com>
2020-09-25 23:37:24 +0900

    settings

--- usdata/settings.py
+++ usdata/settings.py
@@ -6,6 +6,11 @@ load_dotenv(verbose=True)
 
 # env variables
 DJANGO_SECRET = os.getenv('DJANGO_SECRET')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PW = os.getenv('DB_PW')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET
@@ -61,8 +66,12 @@ WSGI_APPLICATION = 'usdata.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PW,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
     }
 }
 
